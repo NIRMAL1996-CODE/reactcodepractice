ad1138737b7ede35f10b0b550ccce07416d7663d
Nirmal Jaiswal <nirmaljaiswal888@gmail.com>
2025-04-07 23:17:02 +0530

    Learning react with tailwind

---
+++ reactWithTailwindcss/src/App.tsx
@@ -0,0 +1,20 @@
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-green-300 text-6xl text-center border">Vite + React + TypeScript + Tailwindcss</h1>
+      <h3 className="text-pink-100 text-6xl text-center">Hello World</h3>
+      <h3 className="text-pink-200 text-6xl text-center">Hello World</h3>
+      <h3 className="text-pink-300 text-6xl text-center">Hello World</h3>
+      <h3 className="text-pink-400 text-6xl text-center">Hello World</h3>
+      <h3 className="text-pink-500 text-6xl text-center">Hello World</h3>
+      <h3 className="text-pink-600 text-6xl text-center">Hello World</h3>
+      <h3 className="text-pink-700 text-6xl text-center">Hello World</h3>
+      <h3 className="text-pink-800 text-6xl text-center">Hello World</h3>
+      <h3 className="text-pink-900 text-6xl text-center">Hello World</h3>
+
+    </div>
+  )
+}
+
+export default App;
