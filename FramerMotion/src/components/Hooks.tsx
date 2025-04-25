cf3450473b8983b4ddc0f7b25fe6789b37211c8d
Nirmal Jaiswal <nirmaljaiswal888@gmail.com>
2025-04-25 23:37:02 +0530

    Use useSpring Hook with usemotion Hook in this Slider to animate scaling of motion.div

--- FramerMotion/src/components/Hooks.tsx
+++ FramerMotion/src/components/Hooks.tsx
@@ -1,14 +1,16 @@
-import {motion, useMotionValue} from "framer-motion"
+import {motion, useMotionValue, useSpring} from "framer-motion"
 import { ChangeEvent } from "react";
 const RangeSlider = () => {
   const scale =useMotionValue(1);
-
+  //useMotionValue(1) → Creates a dynamic, animatable value starting at 1.
+  const spring =useSpring(scale);
+  //useSpring(scale, { stiffness: 100 }) → Smoothly animates scale using spring physics (bouncy effect).
   const changeHandler=(e:ChangeEvent<HTMLInputElement>)=>{
   scale.set(parseFloat(e.target.value));
   };
   return (
     <div>
-      <motion.div className="box border-1" style={{scale}}></motion.div>
+      <motion.div className="box" style={{scale:spring}}></motion.div>
           <input type="range"
           min={0.5}
           max={5}
