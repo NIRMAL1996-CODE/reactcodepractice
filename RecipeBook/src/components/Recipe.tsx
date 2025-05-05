752a4b4c489a0539532ae8b597b86d0a321bdc2a
Nirmal Jaiswal <nirmaljaiswal888@gmail.com>
2025-05-05 21:33:28 +0530

    RecipeBook UI

---
+++ RecipeBook/src/components/Recipe.tsx
@@ -0,0 +1,33 @@
+
+const Recipe = () => {
+  return (
+    <>
+      <div className="w-[500px] h-[380px] gap-1 flex flex-col items-center m-1 p-1 rounded-lg shadow-lg bg-white">
+        <h1 className="text-2xl text-green-700 font-bold m-1 p-1 mt-4">Recipe book</h1>
+
+        <div className="flex flex-col justify-center gap-1">
+        <input type="text" 
+         placeholder="Recipe Name" 
+         className="cursor-pointer text-sm border border-gray-200 w-[450px] rounded p-2  placeholder:text-gray-400 placeholder:opacity-70"/>
+        <input type="text" 
+         placeholder="Ingredients(comma separated)" 
+         className="cursor-pointer text-sm border border-gray-200 w-[450px] rounded p-2  placeholder:text-gray-400 placeholder:opacity-70"/>
+        <input type="text" 
+         placeholder="Instructions" 
+         className="cursor-pointer text-sm border border-gray-200 w-[450px] rounded p-2  placeholder:text-gray-400 placeholder:opacity-70"/>
+        </div>
+
+        <div className="w-[450px] flex justify-start">
+        <button className="text-1xl rounded shadow-lg text-white m-1 p-1 bg-green-500 hover:bg-red-400 cursor-pointer">Add Recipe</button>
+        </div>
+
+        <div className="flex justify-end items-center m-1 p-1 w-[450px] h-[100px] rounded bg-green-100">
+        <button className="rounded shadow-lg text-sm text-white m-1 mt-15 p-1 bg-blue-600 hover:bg-blue-400 cursor-pointer">Edit</button>
+        <button className="rounded shadow-lg text-sm text-white m-1 mt-15 p-1 bg-red-600 hover:bg-red-400 cursor-pointer">Delete</button>
+        </div>
+      </div> 
+    </>
+  )
+}
+
+export default Recipe
