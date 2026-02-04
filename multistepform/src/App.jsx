0f341a010e9f05b8b61afa2d84b4160409e272bb
Nirmal Jaiswal <nirmaljaiswal888@gmail.com>
2026-02-04 11:31:55 +0530

    updated

---
+++ multistepform/src/App.jsx
@@ -0,0 +1,29 @@
+
+import CheckOutStepper from './components/CheckOutStepper'
+
+const CHECKOUT_STEPS=[ 
+
+{name: "Customer Info",
+   Component: ()=> <div>Provide your contact details.</div>},
+
+{name: "Shipping Info",
+   Component: ()=> <div>Enter your shipping address.</div>},
+
+{name: "Payment",
+   Component: ()=> <div>Complete payment for your order.</div>},
+
+{name: "Delivered",
+   Component: ()=> <div>Your order has been delivered.</div>}
+
+];
+
+const App = () => {
+  return (
+    <div>
+      <h2>CheckOut</h2>
+      <CheckOutStepper stepConfig = {CHECKOUT_STEPS}/>
+    </div>
+  )
+}
+
+export default App
